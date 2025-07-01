b482ced132d08f26bb42dd5def970a187d668914
LianBimix <lianbimix@gmail.com>
2025-07-01 11:31:44 +0200

    Quiz and better folders

--- Quiz/Data.js
+++ Quiz/Data.js
@@ -1,19 +1,52 @@
 "use strict";
-var Quiz;
-(function (Quiz) {
-    let data = {};
-    let answer = {};
-    let solution = {};
-    question: "What is the capital of France?",
-        answer;
-    [
-        "Paris",
-        "Berlin",
-        "Madrid",
-        "Rome",
-        "London"
-    ],
-        solution;
-    [1];
-})(Quiz || (Quiz = {}));
-//# sourceMappingURL=Data.js.map
+var Quizz;
+(function (Quizz) {
+    Quizz.data = {
+        question1: "What is the capital of Germany? ;)",
+        answer1: [
+            "Paris",
+            "Berlin",
+            "Furtwangen",
+            "Rome",
+            "London",
+        ],
+        solution1: [2],
+        question2: "What drink do the majority of GMB students drink?",
+        answer2: [
+            "Tea",
+            "Energy Drink",
+            "Water",
+            "Soda",
+            "Coffee",
+        ],
+        solution2: [1, 4],
+        question3: "How many paper figures are in the M-Bau?",
+        answer3: [
+            "2",
+            "5",
+            "3",
+            "4",
+            "there are no paper figures!",
+        ],
+        solution3: [],
+        question4: "When does the Mensa open and close?",
+        answer4: [
+            "8:00 - 13:45",
+            "11:00 - 14:00",
+            "11:00 - 14:00",
+            "11:15 - 15:00",
+            "11:15 - 13:45",
+            "11:30 - 13:45",
+            "10:30 - 13:00",
+        ],
+        solution4: [4],
+        question5: "Monster energy, Rockstar or Red Bull? (The answer was evaluated by a survey of 7 Students)",
+        answer5: [
+            "Monster",
+            "Rockstar",
+            "Red Bull",
+            "None of them",
+        ],
+        solution5: [2],
+    };
+})(Quizz || (Quizz = {}));
--- Quiz/Data.ts
+++ Quiz/Data.ts
@@ -1,16 +1,55 @@
-namespace Quiz {
-    let data: { [key: string]: any } = {};
+namespace Quizz {
+    export let data = {
 
-    let answer: { [key: string]: any } = {};
-
-    let solution: { [key: string]: any } = {};
-
-    question:"What is the capital of France?",
-    answer: [
+    question1:"What is the capital of Germany? ;)",
+    answer1: [
         "Paris",
         "Berlin",
-        "Madrid",
+        "Furtwangen",
         "Rome", 
-        "London"],
-    solution: [1];
+        "London",
+    ],
+    solution1: [2],
+
+    question2: "What drink do the majority of GMB students drink?",
+    answer2: [
+        "Tea",
+        "Energy Drink",
+        "Water",
+        "Soda",
+        "Coffee",
+    ],
+    solution2:[1, 4],
+
+    question3: "How many paper figures are in the M-Bau?",
+    answer3: [
+        "2",
+        "5",
+        "3",
+        "4",
+        "there are no paper figures!",
+    ], 
+    solution3: [],
+
+    question4: "When does the Mensa open and close?",
+    answer4: [
+        "8:00 - 13:45",
+        "11:00 - 14:00",
+        "11:00 - 14:00",
+        "11:15 - 15:00",
+        "11:15 - 13:45",
+        "11:30 - 13:45",
+        "10:30 - 13:00",
+    ], 
+    solution4: [4],
+
+    question5: "Monster energy, Rockstar or Red Bull? (The answer was evaluated by a survey of 7 Students)",
+    answer5: [
+        "Monster",
+        "Rockstar",
+        "Red Bull",
+        "None of them",
+    ], 
+    solution5: [2],
+    }
 }
