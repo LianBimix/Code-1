52c138a92885308d005ecfbb2481bde7df0d4940
LianBimix <lianbimix@gmail.com>
2025-07-01 10:06:47 +0200

    NIM

--- Quiz/Data.ts
+++ Quiz/Data.ts
@@ -1,3 +1,16 @@
 namespace Quiz {
-    let data:
+    let data: { [key: string]: any } = {};
+
+    let answer: { [key: string]: any } = {};
+
+    let solution: { [key: string]: any } = {};
+
+    question:"What is the capital of France?",
+    answer: [
+        "Paris",
+        "Berlin",
+        "Madrid",
+        "Rome", 
+        "London"],
+    solution: [1];
 }
