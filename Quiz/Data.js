33abdf1280e0837abe168dbf833e1d0438f704bc
LianBimix <lianbimix@gmail.com>
2025-07-01 10:15:16 +0200

    NIM (bitteee)

---
+++ Quiz/Data.js
@@ -0,0 +1,19 @@
+"use strict";
+var Quiz;
+(function (Quiz) {
+    let data = {};
+    let answer = {};
+    let solution = {};
+    question: "What is the capital of France?",
+        answer;
+    [
+        "Paris",
+        "Berlin",
+        "Madrid",
+        "Rome",
+        "London"
+    ],
+        solution;
+    [1];
+})(Quiz || (Quiz = {}));
+//# sourceMappingURL=Data.js.map
