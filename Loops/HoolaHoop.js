8b16c3bf6fda7caec1e4e4ce46384a0c37036122
LianBimix <lianbimix@gmail.com>
2025-07-01 16:26:59 +0200

    Loop practice

---
+++ Loops/HoolaHoop.js
@@ -0,0 +1,23 @@
+"use strict";
+//let a=2;
+//let b=12;
+//for (let i:number = b; i > 1; i/=2) {
+//  if (i == 3)
+//    continue;
+//  if (i >= a)
+//    break;
+//  console.log(i);
+//}
+//let c=[4,7,2];
+//
+//for (let j:number=0; j<3; j++) {
+//  console.log(c[j]);  
+//}
+//let o = {x:1, y:2};
+//for (let value of o) {
+//  console.log(o);
+//}
+let o = { x: 1, y: 2 };
+for (let k = 0; k < o.y; k++) {
+    console.log(k);
+}
