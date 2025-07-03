4c4d15bfd1f0eb47ddc6c1a5fdf170d5b83dc1da
LianBimix <lianbimix@gmail.com>
2025-07-03 15:36:49 +0200

    playing around

---
+++ Idk/idk.js
@@ -0,0 +1,8 @@
+"use strict";
+var L02_Load;
+(function (L02_Load) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log(_event);
+    }
+})(L02_Load || (L02_Load = {}));
