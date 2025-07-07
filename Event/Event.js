b64d6d07050c3e2fa4600022b90aa60ec8402009
LianBimix <lianbimix@gmail.com>
2025-07-07 17:33:37 +0200

    Ballz

--- Event/Event.js
+++ Event/Event.js
@@ -1,1 +1,28 @@
 "use strict";
+var Ballz;
+(function (Ballz) {
+    window.addEventListener("load", hndLoad);
+    let balls = [];
+    function hndLoad() {
+        for (let i = 0; i < 10; i++) {
+            const ball = {
+                element: document.createElement("span"),
+                position: { x: 100, y: 100 },
+                velocity: { x: 10, y: 10 },
+            };
+            document.body.appendChild(ball.element);
+            balls.push(ball);
+        }
+        move();
+    }
+    function move() {
+        for (let ball of balls) {
+            ball.position.x -= ball.velocity.x;
+            ball.position.y += ball.velocity.y;
+            ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
+            ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
+            ball.element.style.transform = "matrix(20, 0, 0, 20, " + ball.position.x + ", " + ball.position.y;
+        }
+        setTimeout(move, 16);
+    }
+})(Ballz || (Ballz = {}));
--- Event/Event.ts
+++ Event/Event.ts
@@ -1,3 +1,39 @@
-namespace L02_Load {
-    
+namespace Ballz {
+    type Vector = { x: number, y: number };
+
+    type Ball = {
+        element: HTMLSpanElement,
+        position: Vector,
+        velocity: Vector,
+    }
+
+    window.addEventListener("load", hndLoad);
+    let balls: Ball[] = [];
+
+    function hndLoad(): void {
+        for (let i: number = 0; i<10; i++) {
+            const ball: Ball = {
+                element: document.createElement("span"),
+                position: { x: 100, y: 100 },
+                velocity: { x: 10, y: 10 },
+            }
+            document.body.appendChild(ball.element);
+            balls.push(ball);
+        }
+        move();
+    }
+
+
+    function move(): void {
+        for (let ball of balls) {
+            ball.position.x -= ball.velocity.x;
+            ball.position.y += ball.velocity.y;
+
+            ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
+            ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
+
+            ball.element.style.transform = "matrix(20, 0, 0, 20, " + ball.position.x + ", " + ball.position.y;
+        }
+        setTimeout(move, 16);
+    }
 }
