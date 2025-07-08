67e6a4aebe6f695f8290f7d7e1ae4e5ef5640f2a
LianBimix <lianbimix@gmail.com>
2025-07-08 15:16:12 +0200

    Event (not working for now)

--- Event/Event.js
+++ Event/Event.js
@@ -4,7 +4,7 @@ var Ballz;
     window.addEventListener("load", hndLoad);
     let balls = [];
     let timePreviousFrame = Date.now();
-    document.body.addEventListener;
+    let ballIdCounter = 0;
     function hndLoad() {
         for (let i = 0; i < 100; i++) {
             let newBall = {
@@ -17,24 +17,50 @@ var Ballz;
                     x: (Math.random() - 0.5) * 20,
                     y: (Math.random() - 0.5) * 20,
                 },
+                id: ballIdCounter++,
             };
             document.body.appendChild(newBall.element);
             balls.push(newBall);
+            requestAnimationFrame(update);
         }
-        move();
+        update();
     }
-    function move() {
+    function onClick(_event) {
+        let target = _event.target;
+        if (target.tagName.toLowerCase() == "span" && target.dataset.ballId) {
+            const clickedBallId = parseInt(target.dataset.ballId);
+            if (target.parentNode) {
+                target.parentNode.removeChild(target);
+            }
+            const index = balls.findIndex(ball => ball.id == clickedBallId);
+            if (index > -1) {
+                balls.splice(index, 1);
+            }
+        }
+    }
+    function checkCollitionAll() {
+        for (const a in balls) {
+            for (let b = Number(a) + 1; b < balls.length; b++)
+                ;
+        }
+    }
+    function update() {
         const timeCurrent = Date.now();
         let timeDelta = timeCurrent - timePreviousFrame;
         timeDelta /= 50;
+        move(timeDelta);
+        checkCollitionAll();
+        timePreviousFrame = timeCurrent;
+        requestAnimationFrame(update);
+    }
+    function move(_timeDelta) {
+        window.addEventListener("mousedown", onClick);
         for (let ball of balls) {
-            ball.position.x -= ball.velocity.x * timeDelta;
-            ball.position.y += ball.velocity.y * timeDelta;
+            ball.position.x -= ball.velocity.x * _timeDelta;
+            ball.position.y += ball.velocity.y * _timeDelta;
             ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
             ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
             ball.element.style.transform = "matrix(20, 0, 0, 20, " + ball.position.x + ", " + ball.position.y;
         }
-        timePreviousFrame = timeCurrent;
-        requestAnimationFrame(move);
     }
 })(Ballz || (Ballz = {}));
--- Event/Event.ts
+++ Event/Event.ts
@@ -5,49 +5,74 @@ namespace Ballz {
         element: HTMLSpanElement,
         position: Vector,
         velocity: Vector,
+        id: number;
     }
 
     window.addEventListener("load", hndLoad);
     let balls: Ball[] = [];
     let timePreviousFrame: number = Date.now();
-    document.body.addEventListener
+    let ballIdCounter: number = 0;
 
     function hndLoad(): void {
-        for (let i: number = 0; i<100; i++) {
+        for (let i: number = 0; i < 100; i++) {
             let newBall: Ball = {
                 element: document.createElement("span"),
-                position: { 
-                    x: Math.random()*window.innerWidth,
-                    y: Math.random()*window.innerHeight,
+                position: {
+                    x: Math.random() * window.innerWidth,
+                    y: Math.random() * window.innerHeight,
                 },
-                velocity: { 
-                    x:(Math.random()-0.5)*20,
-                    y:(Math.random()-0.5)*20,
+                velocity: {
+                    x: (Math.random() - 0.5) * 20,
+                    y: (Math.random() - 0.5) * 20,
                 },
+                id: ballIdCounter++,
             }
             document.body.appendChild(newBall.element);
             balls.push(newBall);
+            requestAnimationFrame(update);
         }
-        move();
+        update();
+    }
+    function onClick(_event: MouseEvent) {
+        let target: HTMLElement = _event.target as HTMLElement;
+
+        if (target.tagName.toLowerCase() == "span" && target.dataset.ballId) {
+            const clickedBallId = parseInt(target.dataset.ballId);
+
+            if (target.parentNode) {
+                target.parentNode.removeChild(target);
+            }
+            const index = balls.findIndex(ball => ball.id == clickedBallId);
+            if (index > -1) {
+                balls.splice(index, 1);
+            }
+        }
+    }
+    function checkCollitionAll(): void {
+        for (const a in balls) {
+            for (let b: number = Number(a) + 1; b < balls.length; b++);
+        }
+    }
+    function update() {
+        const timeCurrent: number = Date.now();
+        let timeDelta: number = timeCurrent - timePreviousFrame;
+        timeDelta /= 50;
+        move(timeDelta);
+        checkCollitionAll();
+        timePreviousFrame = timeCurrent;
+        requestAnimationFrame(update);
     }
 
-
-    function move(): void {
-        const timeCurrent: number = Date.now(); 
-        let timeDelta: number = timeCurrent - timePreviousFrame;
-        timeDelta/=50;
+    function move(_timeDelta: number): void {
+        window.addEventListener("mousedown", onClick);
         for (let ball of balls) {
-            ball.position.x -= ball.velocity.x*timeDelta;
-            ball.position.y += ball.velocity.y*timeDelta;
+            ball.position.x -= ball.velocity.x * _timeDelta;
+            ball.position.y += ball.velocity.y * _timeDelta;
 
             ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
             ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
 
             ball.element.style.transform = "matrix(20, 0, 0, 20, " + ball.position.x + ", " + ball.position.y;
         }
-        timePreviousFrame = timeCurrent;
-        requestAnimationFrame(move);
     }
-
-
 }
